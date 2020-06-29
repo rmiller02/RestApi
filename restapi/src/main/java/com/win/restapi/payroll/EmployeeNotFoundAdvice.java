6e9e7fd0b445f41f98450308dcc4ad64dc174f79
rmiller02 <r_miller02@yahoo.com>
2020-06-29 07:54:49 -0400

    first commit

---
+++ restapi/src/main/java/com/win/restapi/payroll/EmployeeNotFoundAdvice.java
@@ -0,0 +1,13 @@
+package com.win.restapi.payroll;
+
+class EmployeeNotFoundException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
---
+++ restapi/src/main/java/com/win/restapi/payroll/Status.java
@@ -0,0 +1,8 @@
+package com.win.restapi.payroll;
+
+enum Status {
+
+    IN_PROGRESS, //
+    COMPLETED, //
+    CANCELLED
+}
